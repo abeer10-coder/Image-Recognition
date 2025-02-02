361a2462b82021720dbec1a476dd2e360b26ed1a
abeer-10 <saptarshiabeer2004@gmail.com>
2025-02-03 01:59:13 +0530

    Adding comment to understand

--- Brain.py
+++ Brain.py
@@ -1,3 +1,10 @@
+# First "clone" git repo in our system
+# Second we move our files in .git folder
+# Third We check the "status", if not not added to repo then,
+# Fourth We use "git add <file> or git add ." to add files in load ing process one by one or all at once respectively.
+# Fifth We us "git commit -m 'random msg'" to commit the changes in the file with a message (optional)
+# Sixth We use "git push" to upload in the repo final step
+
 from imageai.Classification import ImageClassification
 import os
 
